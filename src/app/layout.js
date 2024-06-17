1717a43ddd58629372b0c1d0162c9245f3df175d
Ralfh Bryan Perez <ralfhbryanperez@yahoo.com>
2024-06-17 23:12:12 +0800

    fix bootstrap client

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./globals.css";
 import { Inter } from "next/font/google";
-import BootstrapClient from '@/components/BootstrapClient.js';
+import BootstrapClient from "./components/BootstrapClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
